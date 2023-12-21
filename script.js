4c196dd728b0c992a91cda4d38b6ddd6c80672cc
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2023-12-21 14:26:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
-// your code here
+	// your code here
+	if(num = 1) return 0;
+	if(num = 2) return 1;
+
+	let count = num -2;
+	let cur = 1;
+	let prev = 0;
+
+	for(int i=0; i<count; i++) {
+		let sum = cur+prev;
+		prev = cur;
+		cur = sum;
+	}
+	return curr;
 }
 
 module.exports = fibonacci;
